a1140076c1d3b86b0098ab1c4f7ae5fdadc8ff05
Gerson Severo <gersonsevero90@gmail.com>
2021-07-17 19:54:55 -0300

    Creating Pages, Components and installing React Router Dom

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import List from "./components/List";
+
+function Main() {
+  return (
+    <div className="main">
+      <Header />
+      <List />
+    </div>
+  );
+}
+
+export default Main;
